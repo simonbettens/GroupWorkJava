35a5b0dee1b807025903804944c86b27db32fd1d
AaronS <sysaaron@gmail.com>
2020-04-04 18:07:49 +0200

    Added SessieKalenderDao + named query in SessieKalender

--- src/domein/SessieKalender.java
+++ src/domein/SessieKalender.java
@@ -4,6 +4,15 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "SessieKalender.findByBeginJaar",
+				query = "select k from SessieKalender k where k.BeginJaar = :beginJaar")
+})
+
 public class SessieKalender {
 
 	private int id;
@@ -20,6 +29,34 @@ public class SessieKalender {
 		this.startDatum = startDatum;
 		this.eindDatum = eindDatum;
 		this.sessies = new ArrayList<Sessie>();
+	}
+
+	private void setStartDatum(LocalDate startDatum) {
+		this.startDatum = startDatum;
+	}
+
+	private void setEindDatum(LocalDate eindDatum) {
+		this.eindDatum = eindDatum;
+	}
+
+	private void setSessies(List<Sessie> sessies) {
+		this.sessies = sessies;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public LocalDate getStartDatum() {
+		return startDatum;
+	}
+
+	public LocalDate getEindDatum() {
+		return eindDatum;
+	}
+
+	public List<Sessie> getSessies() {
+		return sessies;
 	}
 	
 	
