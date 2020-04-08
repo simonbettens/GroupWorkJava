5ba55abe049d3049869c5cff24cd4ac346029297
AaronS <sysaaron@gmail.com>
2020-04-09 00:18:35 +0200

    Fixed autofocus

--- src/gui/LoginSchermController.java
+++ src/gui/LoginSchermController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -26,81 +25,91 @@ public class LoginSchermController extends AnchorPane {
 			throw new RuntimeException(e);
 		}
 	}
-    @FXML
-    private ImageView imageItLab;
+	
+	    @FXML
+	    private ImageView imageItLab;
 
-    @FXML
-    private TextField fieldUsername;
+	    @FXML
+	    private AnchorPane rightAnchorPane;
 
-    @FXML
-    private PasswordField fieldPassword;
+	    @FXML
+	    private TextField fieldUsername;
 
-    @FXML
-    private Button buttonLogin;
+	    @FXML
+	    private PasswordField fieldPassword;
 
-    @FXML
-    private Hyperlink hlForgotPassword;
+	    @FXML
+	    private Button buttonLogin;
 
-    @FXML
-    private ImageView imageGroup;
+	    @FXML
+	    private Hyperlink hlForgotPassword;
 
-    @FXML
-    private Text textUserLogin;
+	    @FXML
+	    private ImageView imageGroup;
 
-    @FXML
-    private ImageView imagePerson;
+	    @FXML
+	    private Text textUserLogin;
 
-    @FXML
-    private ImageView imageLock;
+	    @FXML
+	    private ImageView imagePerson;
 
-    @FXML
-    void forgotPassword(MouseEvent event) {
-    	
-    }
+	    @FXML
+	    private ImageView imageLock;
 
-    @FXML
-    void hoverExitForgotPassword(MouseEvent event) {
+	    @FXML
+	    void forgotPassword(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverExitLock(MouseEvent event) {
+	    @FXML
+	    void hoverExitForgotPassword(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverExitLogin(MouseEvent event) {
+	    @FXML
+	    void hoverExitLock(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverExitPerson(MouseEvent event) {
+	    @FXML
+	    void hoverExitLogin(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverForgotPassword(MouseEvent event) {
+	    @FXML
+	    void hoverExitPerson(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverLock(MouseEvent event) {
+	    @FXML
+	    void hoverForgotPassword(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverLogin(MouseEvent event) {
+	    @FXML
+	    void hoverLock(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void hoverPerson(MouseEvent event) {
+	    @FXML
+	    void hoverLogin(MouseEvent event) {
 
-    }
+	    }
 
-    @FXML
-    void login(MouseEvent event) {
+	    @FXML
+	    void hoverPerson(MouseEvent event) {
 
-    }
+	    }
 
-}
+	    @FXML
+	    void login(MouseEvent event) {
+
+	    }
+
+	    @FXML
+	    void rightPaneClicked(MouseEvent event) {
+
+	    }
+
+	}
+
